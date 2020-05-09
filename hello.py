34a93bf5142b70fd98f6ff5cbd15b0fee1d20d97
Ivanvukk <ivan.vukovac.iv@gmail.com>
2020-05-09 21:20:56 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print('                  ')
+print('    . .   . .     ')
+print('   .   . .   .    ')
+print('   .    .    .    ')
+print('    .  M+I  .    ')
+print('     .     .    ')
+print('      .   .    ')
+print('       . .    ')
+print('        .    ')
+
+for x in range(100000):
+	print('Micka prasica')
